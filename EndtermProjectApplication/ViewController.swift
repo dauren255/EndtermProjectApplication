893f67e794ab0ca5cbd6838ce173409c822851ac
Dauren <dauren.buribekov@gmail.com>
2020-04-25 23:29:16 +0600

    Initial Commit

---
+++ EndtermProjectApplication/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EndtermProjectApplication
+//
+//  Created by Бурибеков Даурен on 4/25/20.
+//  Copyright © 2020 Бурибеков Даурен. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
